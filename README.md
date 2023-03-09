38bff4eae0c063daaebc074b2b4ddad8fa92f646
solamsa <solamnyangiwe@gmail.com>
2023-03-09 20:30:03 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # perfect_number
+perfect number, a positive integer that is equal to the sum of its proper divisors. The smallest perfect number is 6, which is the sum of 1, 2, and 3. Other perfect numbers are 28, 496, and 8,128.
--- perfect_num.py
+++ perfect_num.py
@@ -1,13 +1,13 @@
 def is_perfect_num(num):
+    """arguments:   Function take the number to be checked
+                    if it is a perferct or not
+        returns: (boolean) true if it is perfect else false"""
     total = 1
-    ls = []
     if num <= 1:
         return False
-    
     for i in range(2,num):
         if num % i == 0:
             total += i
-    
     if total == num:
         return True
     return False
